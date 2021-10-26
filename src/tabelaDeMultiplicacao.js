b937bf1d35667ae9f0ae051d0ae288450c9cc8b4
Fernanda Constância <91707875+fernandaconstancia@users.noreply.github.com>
2021-10-26 00:19:15 -0300

    TabelaHTMLMultiplica

--- src/tabelaDeMultiplicacao.js
+++ src/tabelaDeMultiplicacao.js
@@ -1,9 +1,13 @@
-function nomeDaFuncao() { 
+function tabelaMulti (n = 10) {
     // Substitua "nomeDaSuaFuncao" por um nome mais apropriado
     // TODO: escrever dois loops aninhados para desenhar o array bidimensional e monstar o restante da tabela no console.
-    let x = [];
-    for (let i = 0; i <= 10; i++) {
-        x[i] = [];
+    let x = []
+    for (let linha = 0; linha <= n; linha++){                
+        x[linha] = [];
+        for (let coluna = 0; coluna <= n; coluna++){                
+            x[linha][coluna] = linha * coluna
+        }
     }
     console.table(x)
+
 }
